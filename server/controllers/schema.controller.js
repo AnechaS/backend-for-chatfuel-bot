8dd60836c15eda3f40f1e8bbe86e222dda18da89
Anecha Sun <Anachitheu.k@gmail.com>
2021-02-04 23:00:11 +0700

    Update schemas API

--- server/controllers/schema.controller.js
+++ server/controllers/schema.controller.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose');
+const { omit } = require('lodash');
 const modelSchema = require('../utils/modelSchema');
+const { BLACKLIST_MODELS } = require('../utils/constants');
 
 /**
  * Get all model schemas
  */
 exports.list = function (req, res) {
-  const results = Object.keys(mongoose.models).map((modelName) => {
-    return modelSchema(modelName);
-  });
+  const results = Object.keys(omit(mongoose.models, BLACKLIST_MODELS)).map(
+    (modelName) => {
+      return modelSchema(modelName);
+    }
+  );
 
   res.json({ results });
 };
